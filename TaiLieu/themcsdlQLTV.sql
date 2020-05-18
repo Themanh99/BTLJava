dc842fc532ee5bc2d2a6452fd8ae2825d2b08850
ken9xkyo <46569845+ken9xkyo@users.noreply.github.com>
2020-05-18 15:23:10 +0700

    Cập nhật lại database

--- TaiLieu/themcsdlQLTV.sql
+++ TaiLieu/themcsdlQLTV.sql
@@ -1,17 +1,10 @@
-
+use QLThuVien
 -- taikhoan admin
 insert into TAIKHOAN values 
 ('admin','admin',1),
 ('quang','admin',1),
 ('son','admin',1),
 ('manh','admin',1)
-
-insert into TACGIA values
-('TG001','Do Ngoc Son','1970/01/01'),
-('TG002','Dang Quynh Nga','1971/02/06'),
-('TG003','Tran The Binh','1972/12/25'),
-('TG004','Nguyen Duc Nhat','1973/1/1'),
-('TG005','Phan Van Vien','1970/12/10')
 
 insert into NCC values
 ('NCC001','Nha sach Phuong Nam','0977319769','214 Thanh Xuan'),
@@ -20,20 +13,22 @@ insert into NCC values
 ('NCC004','Nha sach Tuoi Tre','0977319772','132 Cau Dien'),
 ('NCC005','Nha sach Su Pham','0977319773','100 Cau Giay')
 
-insert into NXB values
-('NXB001','Nha xuat ban Kim Dong','0353221111','70 Hoan Kiem Ha Noi'),
-('NXB002','Nha xuat ban Tu Tai','0353220732','Market Vien Lai'),
-('NXB003','Nha xuat ban GD va DT','0353220733','50 Cau Giay'),
-('NXB004','Nha xuat ban KHKT','0353220734','20 Pho Nhon Ha Noi'),
-('NXB005','Nha xuat ban Su Pham','0353220735','Hoang Mai Ha Noi')
 
 
-insert into SACH values 
-('MS0001','toan cao cap1','TG001','NXB001',40000,'sach giao khoa'),
-('MS0002','toan cao cap2','TG001','NXB001',50000,'sach giao khoa'),
-('MS0003','phap luat dc','TG002','NXB002',45000,'sach giao khoa'),
-('MS0004','toan roi rac','TG003','NXB002',55000,'sach giao khoa'),
-('MS0005','sql server','TG004','NXB003',35000,'sach giao khoa')
+
+insert into SACH (maSach,tenSach,tenTacGia,tenNXB,giaSach ,theLoai,trangthai ,soluong )
+values 
+('MS0010' ,'sql server'     ,'Nguyen Duc Nhat' ,'Nha xuat ban KHKT'      ,35000,'sach giao khoa'  ,'false',20),
+('MS0005' ,'sql server'     ,'Nguyen Duc Nhat' ,'Nha xuat ban KHKT'      ,35000,'sach giao khoa'  ,'true' ,20),
+('MS0003' ,'phap luat dc'   ,'Dang Quynh Nga'  ,'Nha xuat ban Su Pham'   ,45000,'sach giao khoa'  ,'true' ,54),
+('MS0008' ,'phap luat dc'   ,'Dang Quynh Nga'  ,'Nha xuat ban Su Pham'   ,45000,'sach nguoi lon'  ,'false',54),
+('MS0004' ,'toan roi rac'   ,'Phan Van Vien'   ,'Nha xuat ban Tu Tai'    ,55000,'sach giao khoa'  ,'false',24),
+('MS0009' ,'toan roi rac'   ,'Phan Van Vien'   ,'Nha xuat ban Tu Tai'    ,55000,'sach giao khoa'  ,'true' ,24),
+
+('MS0001' ,'toan cao cap1'  ,'Do Ngoc Son'     ,'Nha xuat ban GD va DT'  ,40000,'sach giao khoa'  ,'true' ,74),
+('MS0006' ,'toan cao cap1'  ,'Do Ngoc Son'     ,'Nha xuat ban GD va DT'  ,40000,'sach giao khoa'  ,'false',34),
+('MS0002' ,'toan cao cap2'  ,'Tran The Binh'   ,'Nha xuat ban Kim Dong'  ,50000,'sach tre em'     ,'false',46),
+('MS0007' ,'toan cao cap2'  ,'Tran The Binh'   ,'Nha xuat ban Kim Dong'  ,50000,'sach giao khoa'  ,'true' ,46)
 
 insert into SINHVIEN values
 ('2017601707','Chu The Manh','themanh@gmail.com','0345574951'),
@@ -43,22 +38,6 @@ insert into SINHVIEN values
 ('2017601711','Tran Tuan Anh','tuananh@gmail.com','0345574955')
 
 
-
-
-insert into SACHBAN values
-('MS0001',100),
-('MS0002',150),
-('MS0003',160),
-('MS0004',170),
-('MS0005',200)
-
-insert into SACHMUON values
-('MS0001',50),
-('MS0002',50),
-('MS0003',60),
-('MS0004',70),
-('MS0005',100)
-
 insert into PHIEUMUA values
 ('PM0001','2020/11/05','2017601707'),
 ('PM0002','2020/12/05','2017601707'),
@@ -66,19 +45,29 @@ insert into PHIEUMUA values
 ('PM0004','2020/7/05','2017601710'),
 ('PM0005','2020/1/05','2017601709')
 
-insert into CHITIETMUA values
-('PM0001','MS0001',1),
-('PM0002','MS0004',1),
-('PM0003','MS0002',2),
-('PM0004','MS0003',1),
-('PM0005','MS0001',3)
 
 insert into PHIEUNHAP values
-('PN0001','01/04/2020','NCC001'),
-('PN0002','01/04/2020','NCC002'),
-('PN0003','01/05/2020','NCC003'),
-('PN0004','01/05/2020','NCC001'),
-('PN0005','01/05/2020','NCC004')
+('PN0001','2020/01/04','NCC001'),
+('PN0002','2020/01/04','NCC002'),
+('PN0003','2020/01/05','NCC003'),
+('PN0004','2020/01/05','NCC001'),
+('PN0005','2020/01/05','NCC004')
+
+
+
+insert into PHIEUMUONTRA values
+('PM0001','2019/04/05','2017601708'),
+('PM0002','2019/04/05','2017601707'),
+('PM0003','2019/04/05','2017601709'),
+('PM0004','2019/05/05','2017601707'),
+('PM0005','2019/05/05','2017601709')
+
+insert into CHITIETMUONTRA values
+('PM0001','MS0001',1,'',20000),
+('PM0002','MS0002',2,'',30000),
+('PM0003','MS0003',1,'',24000),
+('PM0004','MS0005',5,'',28000),
+('PM0005','MS0001',7,'',40000)
 
 insert into CHITIETPN values
 ('PN0001','MS0001',300,39000),
@@ -87,23 +76,10 @@ insert into CHITIETPN values
 ('PN0004','MS0004',300,54000),
 ('PN0005','MS0005',300,34000)
 
-insert into PHIEUMUON values
-('PM0001','2019/04/05','2017601708'),
-('PM0002','2019/04/05','2017601707'),
-('PM0003','2019/04/05','2017601709'),
-('PM0004','2019/05/05','2017601707'),
-('PM0005','2019/05/05','2017601709')
 
-insert into CHITIETMUON values
-('PM0001','MS0001',1),
-('PM0002','MS0002',1),
-('PM0003','MS0003',1),
-('PM0004','MS0005',1),
-('PM0005','MS0001',1)
-
-insert into PHIEUTRA values
-('PT0001',getdate(),'2017601708','PM0001'),
-('PT0002',getdate(),'2017601707','PM0002'),
-('PT0003',getdate(),'2017601709','PM0003'),
-('PT0004',getdate(),'2017601709','PM0004'),
-('PT0005',getdate(),'2017601709','PM0005')
+insert into CHITIETMUA values
+('PM0001','MS0001',1,5000),
+('PM0002','MS0004',1,6000),
+('PM0003','MS0002',2,7000),
+('PM0004','MS0003',1,12000),
+('PM0005','MS0001',3,24000)
